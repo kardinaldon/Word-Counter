10922500c4a041f3e474246b9ccb638c9d4af888
Andrew <kardinalmail@gmail.com>
2021-04-01 15:34:07 +0300

    service Word Counter, starter

--- src/main/java/service/WordCounter.java
+++ src/main/java/service/WordCounter.java
@@ -1,5 +1,33 @@
 package service;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
 public class WordCounter {
+
+    private static final String fileName = "lorem_ipsum.txt";
+
+    public static void wordCount() {
+
+        SparkConf sparkConf = new SparkConf()
+                .setMaster("local")
+                .setAppName("Word Counter");
+
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+
+        JavaRDD<String> inputFile = sparkContext.textFile(fileName);
+
+        JavaRDD<String> wordsFromFile = inputFile
+                .flatMap(content -> Arrays.asList(content.split(" ")).iterator());
+
+        JavaPairRDD countData = wordsFromFile.mapToPair(t -> new Tuple2(t, 1)).reduceByKey((x, y) -> (int) x + (int) y);
+
+        countData.saveAsTextFile("outputData");
+    }
 
 }
--- src/main/java/starter/Starter.java
+++ src/main/java/starter/Starter.java
@@ -1,5 +1,12 @@
 package starter;
 
+import service.WordCounter;
+
 public class Starter {
 
+    public static void main(String[] args) {
+
+        new WordCounter().wordCount();
+
+    }
 }
